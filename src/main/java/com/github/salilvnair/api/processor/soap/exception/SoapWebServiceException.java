9d629ba106edf8d820c4c6e2a4bd5dcb36a33b06
salilvnair <salilvnairgithub@gmail.com>
2024-08-26 13:32:55 -0500

    added FaultAwareWsMsg in SoapWebServiceException

--- src/main/java/com/github/salilvnair/api/processor/soap/exception/SoapWebServiceException.java
+++ src/main/java/com/github/salilvnair/api/processor/soap/exception/SoapWebServiceException.java
@@ -2,11 +2,13 @@ package com.github.salilvnair.api.processor.soap.exception;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.ws.FaultAwareWebServiceMessage;
 
 @Getter
 public class SoapWebServiceException extends Exception {
     @Setter
     private String webserviceName;
+    private FaultAwareWebServiceMessage faultAwareWebServiceMessage;
     private String faultString;
     public SoapWebServiceException(String s) {
         super(s);
@@ -19,6 +21,12 @@ public class SoapWebServiceException extends Exception {
     public SoapWebServiceException(Throwable cause, String webserviceName) {
         super(cause);
         this.webserviceName = webserviceName;
+    }
+
+    public SoapWebServiceException(Throwable cause, String webserviceName, FaultAwareWebServiceMessage faultAwareWebServiceMessage) {
+        super(cause);
+        this.webserviceName = webserviceName;
+        this.faultAwareWebServiceMessage = faultAwareWebServiceMessage;
     }
 
     public SoapWebServiceException(Throwable cause, String webserviceName, String faultString) {
